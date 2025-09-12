8792f3fd6b607c373f1c9082cee774d908ba9727
avmatthew <vishalavinashm@gmail.com>
2025-09-12 16:01:46 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,22 @@
+const products = [
+    {
+        id: 1,
+        title: "Primeum Almonds",
+        Price: "₹499",
+        Weight: "250g",
+        description: "Enjoy the perfect crunch and rich, nutty flavor of our Premium Almonds, sourced directly from trusted organic farms. Packed with protein, fiber, healthy fats, and essential nutrients, these almonds are a wholesome snack for any time of day.",
+        product: [
+            {
+                name: "Premium Almonds",
+                mainImage: "images/premium almonds.jpg",
+                thumbnails: [
+                    "images/pumpkin.jpg",
+                    "images/2 pack of almonds chawas.webp",
+                    "images/black raisins.jpg"
+                ],
+                Weight: ["100g", "250g", "500g"]
+            }
+        ]
+
+    }
+]
